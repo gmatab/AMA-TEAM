26d80012f2000bdeccd8cbcb786be562b46695ac
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-10-22 22:26:05 +0100

    new project

---
+++ bookstore/src/Etudiant.java
@@ -0,0 +1,33 @@
+
+
+public class Etudiant {
+	 String id ;
+	 String fname ;
+	 String lastname ;
+	 String email ;
+	 public String getid (){
+		 return id;
+	 }
+	 public void setid (String id){
+		 this.id=id;
+	 }
+	 public String getfname (){
+		 return id;
+	 }
+	 public void setfname (String fname){
+		 this.fname=fname;
+	 }
+	 public String getlastname (){
+		 return lastname ;
+	 }
+	 public void setlastname (String lastname ){
+		 this.lastname =lastname ;
+	 }
+	 public String getemail (){
+		 return email;
+	 }
+	 public void setemail (String email){
+		 this.email=email;
+	 }
+
+}
