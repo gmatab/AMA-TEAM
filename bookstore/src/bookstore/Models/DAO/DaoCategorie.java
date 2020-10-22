c1c94cdfea5405eb022eac898edf6fcf4cc512c8
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-10-22 22:32:45 +0100

    com 1

---
+++ bookstore/src/bookstore/Models/DAO/DaoCategorie.java
@@ -0,0 +1,9 @@
+package bookstore.Models.DAO;
+import bookstore.Models.Categorie;
+public class DaoCategorie {
+	Categorie  createcategorie (String titles , int date ,String authors) {
+		return null;
+	};
+	
+
+}
